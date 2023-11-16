948abb4d03eaee61b68b7b4594af616fba4e7059
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-16 14:48:46 +0100

    [ADD]: import variables e array in home php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HomePage</title>
 </head>
+
 <body>
+    @dump($colors)
+    <h1>{{ $saluto }}</h1>
+    <ul>
+        <li>Nome: <strong>{{ $name }}</strong></li>
+        <li>Cognome: <strong>{{ $surname }}</strong></li>
+        @foreach ($colors as $color)
+            <li><strong>{{$color}}</strong></li>
+        @endforeach
+
+    </ul>
 
 </body>
+
 </html>

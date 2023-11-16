f4fa869a9ec621d99a4f3ef12286339e9c8b3086
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-16 15:00:02 +0100

    [ADD]: navbar in home php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+
     <title>HomePage</title>
 </head>
 
 <body>
-    @dump($colors)
-    <h1>{{ $saluto }}</h1>
-    <ul>
-        <li>Nome: <strong>{{ $name }}</strong></li>
-        <li>Cognome: <strong>{{ $surname }}</strong></li>
-        @foreach ($colors as $color)
-            <li><strong>{{$color}}</strong></li>
-        @endforeach
+    <div class="container">
+        <header>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Services</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        {{-- @dump($colors) --}}
+        <h1 class="mt-5">{{ $saluto }}</h1>
+        <ul>
+            <li>Nome: <strong>{{ $name }}</strong></li>
+            <li>Cognome: <strong>{{ $surname }}</strong></li>
+            @foreach ($colors as $color)
+                <li><strong>{{ $color }}</strong></li>
+            @endforeach
 
-    </ul>
+        </ul>
+    </div>
 
 </body>
 

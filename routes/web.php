35f3104588831989fa1951bc32358032f0f7607f
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-16 15:59:14 +0100

    [ADD]: static function get about e services

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,24 @@ Route::get('/', function () {
     $colors = ['rosso', 'giallo', 'verde', 'blu', 'magenta', 'beige', 'arancione'];
     return view('home', compact('saluto', 'name', 'surname', 'colors'));
 });
+
+
+Route::get('/about', function () {
+    $saluto = '"Hello World About"';
+    $collab = 'Bacon Ipsum!';
+    $comment = 'Bacon ipsum dolor amet sirloin pancetta t-bone, andouille turducken beef turkey tri-tip. Biltong prosciutto shankle shoulder pork belly pancetta alcatra cupim frankfurter ball tip burgdoggen capicola salami t-bone. Bresaola corned beef turducken swine, strip steak cow meatball fatback. Flank kevin sausage tri-tip, porchetta tongue corned beef ribeye. Ball tip picanha venison andouille, tail strip steak spare ribs doner ham chuck bacon pork chop beef cow.';
+    $author = 'Mr. Pig';
+
+    return view('about', compact('saluto', 'comment', 'author', 'collab'));
+});
+
+
+Route::get('/services', function () {
+    $saluto = '"Servizi"';
+    $freshProducts = ['Braciole di maiale', 'Fiorentina', 'Costata di manzo', 'Costolette di agnello', 'Costolette di maiale', 'Salsicce fresche dolci e piccanti', 'Macinato di Vitello e Maiale', 'Filetto', 'Arista', 'Pollo intero'];
+    $preparedProducts = ['Pulled Pork', 'Porchetta', 'Alette e cosce di pollo', 'Salsiccia di Maiale secca dolce', 'Salsiccia di Maiale secca piccante', 'Smash Burgers', 'Bacon', 'Guanciale *', 'Prosciutto crudo stagionato', '\'Nduja', 'Arpa di San Venanzo'];
+    $comment = 'Bacon ipsum dolor amet sirloin pancetta t-bone, andouille turducken beef turkey tri-tip. Biltong prosciutto shankle shoulder pork belly pancetta alcatra cupim frankfurter ball tip burgdoggen capicola salami t-bone. Bresaola corned beef turducken swine, strip steak cow meatball fatback. Flank kevin sausage tri-tip, porchetta tongue corned beef ribeye. Ball tip picanha venison andouille, tail strip steak spare ribs doner ham chuck bacon pork chop beef cow.';
+
+
+    return view('services', compact('saluto', 'freshProducts', 'preparedProducts', 'comment'));
+});
